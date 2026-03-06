c66ebb4e68869313ca07fe2bf5944aa9c7470f03
codyeaves78-bit <codyeaves78@gmail.com>
2026-03-06 13:46:21 -0600

    Fix duplicate import of numpy in evaporator.py
    Removed duplicate import of numpy.

--- evaporator.py
+++ evaporator.py
@@ -13,7 +13,6 @@ import pandas as pd
 import numpy as np
 import time
 import math
-import numpy as np
 from scipy.optimize import fsolve
 
 #---------------------------------------------------------------------------------------------------------
